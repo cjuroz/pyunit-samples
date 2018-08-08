f480db8583802ee356cc782daed4c61bd46fdd04
Cristian Uroz <cristian@uroz.cat>
2018-08-08 16:16:30 +0800

    Fix runners for alltests suites

--- alltests.py
+++ alltests.py
@@ -3,3 +3,6 @@ from criticaltests import suite as critical
 from extendedtests import suite as extended
 
 alltests = unittest.TestSuite((critical, extended))
+
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(alltests)
--- alltestsalt.py
+++ alltestsalt.py
@@ -1,9 +1,10 @@
 import unittest
 from numberstest import NumbersTest
 	
+### NOT WORKING AS EXPECTED :( ###	
+	
 loader = unittest.TestLoader()
-suite = unittest.TestSuite()	
-suite.addTests(loader.loadTestsFromModule(NumbersTest))
+suite = loader.loadTestsFromModule(NumbersTest)
 
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
