dee24698b1296766ffbeb89b67b33f2fc42165dd
Cristian Uroz <cristian@uroz.cat>
2018-08-08 14:19:35 +0800

    Add some basic unit tests

---
+++ numbers.py
@@ -0,0 +1,18 @@
+class Numbers():
+
+	def add(self, num1, num2):
+		
+		sum = num1 + num2
+		print("The sum of %s and %s is %s" %(num1, num2, sum))
+		return sum		
+		
+	def divide(self, num1, num2):
+		
+		if num2 == 0:
+			return
+		
+		res = num1 / num2
+		print("The division of %s and %s is %s" %(num1, num2, res))
+		return res
+		
+		
---
+++ numberstest.py
@@ -0,0 +1,32 @@
+import unittest
+from numbers import Numbers
+
+class NumbersTest(unittest.TestCase):
+
+	def test_add(self):
+	
+		num1 = 5
+		num2 = 10
+		total = 15
+		
+		res = Numbers().add(num1, num2)
+		self.assertEqual(total, res)
+		
+	def test_divide(self):
+		num1 = 10
+		num2 = 5
+		total = 2
+		
+		res = Numbers().divide(num1, num2)
+		self.assertEqual(total, res) 
+		
+	def test_divide_byzero(self):
+		num1 = 10
+		num2 = 0
+		total = 2
+		
+		res = Numbers().divide(num1, num2)
+		self.assertTrue(res is None)
+		
+if __name__ == "__main__":
+	unittest.main()
